4c3809ee25a7f58557208145ccaff15a3bfd6d06
rsyd29 <brasyid15@gmail.com>
2021-02-28 20:56:09 +0700

    Pertemuan 1

---
+++ tiket_reservation/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+namespace tiket_reservation
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+            "~/Scripts/jquery.js",
+            "~/Scripts/bootstrap.js",
+            "~/Scripts/jquery.fullpage.js",
+            "~/Scripts/material.js",
+            "~/Scripts/ripples.js"
+            ));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+            "~/Content/css/style.css",
+            "~/Content/css/bootstrap.css",
+            "~/Content/css/bootstrap-material-design.css",
+            "~/Content/css/jquery.fullpage.css",
+            "~/Content/css/ripples.css",
+            "~/Content/css/font-awesome/font-awesome.css"
+            ));
+        }
+    }
+}
---
+++ tiket_reservation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tiket_reservation.MvcApplication" Language="C#" %>
